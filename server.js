6d1c071629db6ecc9c6b142020af515119daaf9c
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-02 11:43:42 +0530

    codes added for port listerner

--- server.js
+++ server.js
@@ -2,3 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+
+mongoose.connect("mongodb://localhost:27017/testdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connect;
+
+db.on("error", (err) => {
+  console.log(err);
+});
+db.once("open", () => {
+  console.log("database connection establishged");
+});
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("server is running on port ${PORT}");
+});
